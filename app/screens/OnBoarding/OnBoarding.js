14d6d71d2b52f34948689c4c90a02a8975ff9f8f
Ankit Hans <ankithans1947@gmail.com>
2020-12-15 21:33:34 +0530

    feat: project setup

---
+++ app/screens/OnBoarding/OnBoarding.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, View, StyleSheet, Text} from 'react-native';
+
+const OnBoarding = () => {
+  return (
+    <SafeAreaView>
+      <Text>OnBoarding Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default OnBoarding;
